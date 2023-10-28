e8be467ea9506a190fe7b7440cbe7d59e1a990a3
juan-francisco.caliz-gonzalez@t-systems.com <juan-francisco.caliz-gonzalez@t-systems.com>
2023-10-28 16:02:13 +0200

    Character movement and basic weight system.

---
+++ scripts/main_chr.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const BASE_SPEED: float = 400.0
+const SPEED_DOWN: float = 70.0
+
+var speed: float
+var weight: float = 0.0
+var time: float = 0.0
+var incr_weight: bool = true
+
+func _physics_process(delta):
+	check_weight(delta) # Weight goes up and down in 2s intervals.
+	speed = BASE_SPEED - SPEED_DOWN * weight
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	
+	move_and_slide()
+
+func weight_up():
+	weight += 1
+
+func weight_down():
+	weight -= 1
+	
+func check_weight(delta):
+	time += delta
+	if time >= 2:
+		if incr_weight:
+			weight_up()
+			print(weight)
+			if weight >= 5: incr_weight = false
+		else:
+			weight_down()
+			print(weight)
+			if weight <= 0: incr_weight = true
+		time = 0
