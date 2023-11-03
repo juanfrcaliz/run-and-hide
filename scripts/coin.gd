2cfaac7b6876d3c01796e911a45715d9692baebd
Juanfrcaliz <35780297+juanfrcaliz@users.noreply.github.com>
2023-11-03 17:31:00 +0100

    Added collisions on wall tiles. Added pickable coin that increases carried weight.

---
+++ scripts/coin.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+var picked: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and body.weight < 5:
+		picked = true
+		get_node("Coin").visible = false
+		body.weight += 1
--- scripts/main_chr.gd
+++ scripts/main_chr.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-const BASE_SPEED: float = 400.0
-const SPEED_DOWN: float = 70.0
+const BASE_SPEED: float = 200.0
+const SPEED_DOWN: float = BASE_SPEED / 6
 
 var speed: float
 var weight: float = 0.0
 var time: float = 0.0
 var incr_weight: bool = true
 
-func _physics_process(delta):
-	check_weight(delta) # Weight goes up and down in 2s intervals.
+func _physics_process(_delta):
+	# check_weight(delta) # Weight goes up and down in 2s intervals.
 	speed = BASE_SPEED - SPEED_DOWN * weight
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
