bcda202865138511d12e1822b9d48a2d74f17865
Juanfrcaliz <35780297+juanfrcaliz@users.noreply.github.com>
2023-11-08 20:28:13 +0100

    Fixed menus for web.

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,4 @@ func _ready():
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/initial_poc.tscn")
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
+	get_tree().paused = false
